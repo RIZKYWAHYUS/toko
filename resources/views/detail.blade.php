4d7c3e175bf422695891628794af3aa7ef36dcc4
dwitamalfi <alfi.dwitama@gmail.com>
2017-06-09 21:31:39 +0700

    modified:   app/Http/Controllers/tokoController.php 	new file:   public/img/Logo.png 	new file:   public/img/Logo1.png 	new file:   public/img/bg2.jpg 	new file:   public/img/bg3.jpg 	new file:   public/img/bg4.jpg 	modified:   resources/views/detail.blade.php 	modified:   resources/views/home1.blade.php 	modified:   resources/views/layouts/app1.blade.php

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -39,11 +39,11 @@ color:white;
         <div class="col-md-12">
         @if(isset($status) AND $status=="berhasil")
           <div class="alert alert-success page-scroll" role="alert">
-          Pembelian berhasil <span class="glyphicon glyphicon-ok"></span> 
+          Pembelian berhasil <span class="glyphicon glyphicon-ok"></span>
           </div>
        @endif
           <div class="alert alert-danger page-scroll hidden" role="alert">
-          Pembelian gagal <span class="glyphicon glyphicon-remove"></span> 
+          Pembelian gagal <span class="glyphicon glyphicon-remove"></span>
           </div>
         </div>
    </div>
@@ -52,8 +52,8 @@ color:white;
 
 <div class="about" >
   <div class="container" >
-      
-         <div class='col-md-3' style=" padding:0;">  
+
+         <div class='col-md-3' style=" padding:0;">
             <div class='panel panel-default'>
               <div class='panel-heading'>
                 <img class='img-responsive' id='img-kosong' src='{{$gbr}}'   style='height:300' alt='aaa'/>
@@ -68,10 +68,10 @@ color:white;
          <span class='label label-success'>Ready Stock</span>
 			@else
 				 <span class='label label-danger'>Kosong</span>
-      @endif												
+      @endif
      <h2><b>{{$nama}} </b></h2>
      <p> {{$harga}}</p>
-     <p>{{ $gbr}}</p>
+     
      <p>Ready  {{$stock}} </p>
     <form action="" method="post">
                    Jumah pembelian :
@@ -84,7 +84,7 @@ color:white;
           <input type="hidden" name="idbarang" value="{{ $id }}"/>
           <input type='number' class="form-control" name='jumlah' min='1' max='100' step='1' value='1' style="width:100px;"/><br>
           <input type="submit" class="btn btn-success" name="submit" value="Beli Sekarang"/>
-          <input type="submit" class="btn btn-primary" name="submit" value="Tambahkan Ke Daftar Belanja"/> 
+          <input type="submit" class="btn btn-primary" name="submit" value="Tambahkan Ke Daftar Belanja"/>
     </form>
 </div>
 
@@ -104,11 +104,11 @@ color:white;
         <b>Email</b><br>
         {{ $emailpenjual }}
       </p>
-      <p>No. HP<br>    
-       {{ $nohppenjual }}  
+      <p>No. HP<br>
+       {{ $nohppenjual }}
       </p>
       <p>Bergabung<br>
-      {{ $tglpenjual }}  
+      {{ $tglpenjual }}
       </p>
     </center>
 
@@ -123,4 +123,3 @@ color:white;
 </div>
 
 @endsection
-
